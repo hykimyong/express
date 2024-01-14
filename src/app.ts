4f63e933303272c0f40f46fedc78508740bbef9d
hykimyong <hykimyong@naver.com>
2024-01-14 16:14:13 +0900

    post 요청 테스트

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,15 @@ import * as express from "express";
 
 const app: express.Express = express();
 
-const port: number = 8000;
+const port: number = 3000;
 
-app.get("/", (req: express.Request, res: express.Response) => {
+app.get("/test", (req: express.Request, res: express.Response) => {
   console.log(req);
   res.send({ name: "kim nak yong", age: 99, friends: ["ss", "ys", "ye"] });
+});
+
+app.post("/test", (req, res) => {
+  res.send({ person: "kim" });
 });
 
 app.listen(port, () => {
