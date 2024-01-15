28b2c7610b486d4c9f97d1b8b8af4df27bc2cb6e
hykimyong <hykimyong@naver.com>
2024-01-15 21:59:23 +0900

    예시 API 생성

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,18 @@ import { Cat, CatType } from "./app.model";
 const app: express.Express = express();
 
 app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req);
+  console.log(req.rawHeaders[1]);
   res.send({ cats: Cat });
+});
+
+app.get("/cats/blue", (req, res) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ blue: Cat[0] });
+});
+
+app.get("/cats/som", (req, res) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ som: Cat[1] });
 });
 
 app.listen(8000, () => {
