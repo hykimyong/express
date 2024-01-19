2c3f0968d052cd35c722f8e05f58ac07516ef348
hykimyong <hykimyong@naver.com>
2024-01-19 23:22:46 +0900

    고양이데이터 생성

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,55 @@
 import * as express from "express";
-import { Cat, CatType } from "./app.model";
+import { Cat } from "./app.model";
 
 const app: express.Express = express();
 
+// loggin middleware
 app.use((req, res, next) => {
   console.log(req.rawHeaders[1]);
   console.log("this is logging middleware");
   next();
 });
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req.rawHeaders[1]);
-  res.send({ cats: Cat });
+// READ 고양이 전체 데이터 조회
+app.get("/cats", (req, res) => {
+  try {
+    const cats = Cat;
+    res.send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
-app.get("/cats/blue", (req, res) => {
-  console.log(req.rawHeaders[1]);
-  res.send({ blue: Cat[0] });
+//특정 고양이 데이터 조회
+app.get("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const cats = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    res.send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
-app.get("/cats/som", (req, res) => {
-  console.log(req.rawHeaders[1]);
-  res.send({ som: Cat[1] });
-});
-
+//404 middleware
 app.use((req, res, next) => {
   console.log("this is logging middleware");
   res.send({ error: "404 not found" });
